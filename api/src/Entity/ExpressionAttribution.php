31b1c4b8302eb4e1da37bf9eefbb5ca97590eba2
Tiziana Mancinelli <tiziana.mancinelli@gmail.com>
2019-05-05 10:19:53 +0200

    Attributed Works and Expressions

---
+++ api/src/Entity/ExpressionAttribution.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiProperty;
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+/**
+ * This is the Expression Attribution class.
+ *
+ * @ApiResource(
+ *     collectionOperations={
+ *         "get",
+ *         "post"={"access_control"="is_granted('ROLE_ADMIN')"}
+ *     },
+ *      itemOperations={
+ *         "get",
+ *         "put"={"access_control"="is_granted('ROLE_ADMIN')"},
+ *         "delete"={"access_control"="is_granted('ROLE_ADMIN')"}
+ *     }
+ * )
+ * @ORM\Entity
+ * @UniqueEntity(fields={"attribution", "expression"})
+ */
+class ExpressionAttribution
+{
+    /**
+     * @var int The entity Id
+     *
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+    
+    /**
+     * @var The expression
+     * @ORM\ManyToOne(targetEntity="Expression", inversedBy="attributions")
+     */
+    public $expression;
+    /**
+     * @var The attribution
+     * @ORM\ManyToOne(targetEntity="Person", inversedBy="attributedExpressions")
+     */
+    public $attribution;
+    
+    public function getId(): int
+    {
+        return $this->id;
+    }
+}
